e13c41768ea20dc3f6d2220711bc1a91bf397ceb
timeeehd <timeeehd@gmail.com>
2021-09-24 09:01:29 +0200

    Update in quicksort_alg1

--- main.py
+++ main.py
@@ -16,28 +16,22 @@ def quicksort_sent(array, left, right):
                 if mem >= p:
                     break
             while True:
+                # When removing the sentinel, with python, it still seems to be working fine, bc in python
+                # when j goes negative, it just takes the last element, which in this case is the pivot itself
                 j = j - 1
                 mem = array[j]
                 if mem <= p:
                     break
             if j > i:
-                memory = copy.deepcopy(array)
-                # memory = array
-                array[i] = memory[j]
-                array[j] = memory[i]
+                array[i], array[j] = array[j], array[i]
             elif i > j:
                 break
-        memory = copy.deepcopy(array)
-        # memory = array
-        array[i] = memory[right]
-        array[right] = memory[i]
+        array[i], array[right] = array[right], array[i]
         # array.pop(0)
         # left = left - 1
-        array[left:i] = quicksort_sent(array, left, i - 1)[left:i]
-        array[i + 1:] = quicksort_sent(array, i + 1, right)[i + 1:]
-        return array
-    else:
-        return array
+        quicksort_sent(array, left, i - 1)[left:i]
+        quicksort_sent(array, i + 1, right)[i + 1:]
+    return array
 
 
 def quick_sort_add_sent(array, left, right):
@@ -69,28 +63,20 @@ def quick_sort_no_sent(array, left, right):
                 if mem <= p:
                     break
             if j > i:
-                memory = copy.deepcopy(array)
-                # memory = array
-                array[i] = memory[j]
-                array[j] = memory[i]
+                array[i], array[j] = array[j], array[i]
             elif i > j:
                 break
-        memory = copy.deepcopy(array)
-        # memory = array
-        array[i] = memory[right]
-        array[right] = memory[i]
+        array[i], array[right] = array[right], array[i]
         # array.pop(0)
         # left = left - 1
-        array[left:i] = quicksort_sent(array, left, i - 1)[left:i]
-        array[i + 1:] = quicksort_sent(array, i + 1, right)[i + 1:]
-        return array
-    else:
-        return array
+        quicksort_sent(array, left, i - 1)[left:i]
+        quicksort_sent(array, i + 1, right)[i + 1:]
+    return array
 
 
 if __name__ == '__main__':
     # test = [7,6,5,4,3,2,1]
-    # test = [2,3,1,4,6,7,5]
-    test = [1, 2, 3, 4, 5, 6, 7]
-    sorted_list = quick_sort_no_sent(test, 0, len(test) - 1)
+    test = [2, 3, 1, 4, 6, 7, 5]
+    # test = [1, 2, 3, 4, 5, 6, 7]
+    sorted_list = quick_sort_add_sent(test, 0, len(test) - 1)
     print(sorted_list)
