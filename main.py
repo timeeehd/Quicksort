7dcb1409bb501f4faa70c09b8dd41b461a1fcc9e
timeeehd <timeeehd@gmail.com>
2021-09-24 08:06:33 +0200

    Refactor some names to be clearer

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import copy
 
 
-def quicksort(array, left, right):
+def quicksort_sent(array, left, right):
     # left = left + 1
     if right - left >= 1:
         # array = [-1000000] + array
@@ -22,30 +22,32 @@ def quicksort(array, left, right):
                     break
             if j > i:
                 memory = copy.deepcopy(array)
+                # memory = array
                 array[i] = memory[j]
                 array[j] = memory[i]
             elif i > j:
                 break
         memory = copy.deepcopy(array)
+        # memory = array
         array[i] = memory[right]
         array[right] = memory[i]
         # array.pop(0)
         # left = left - 1
-        array[left:i] = quicksort(array, left, i - 1)[left:i]
-        array[i + 1:] = quicksort(array, i + 1, right)[i + 1:]
+        array[left:i] = quicksort_sent(array, left, i - 1)[left:i]
+        array[i + 1:] = quicksort_sent(array, i + 1, right)[i + 1:]
         return array
     else:
         return array
 
 
-def sort_func(array, left, right):
+def quick_sort_add_sent(array, left, right):
     array = [-1000000] + array
-    sorted_list = quicksort(array, left + 1, right + 1)
+    sorted_list = quicksort_sent(array, left + 1, right + 1)
     sorted_list.pop(0)
     return sorted_list
 
 
 if __name__ == '__main__':
     test = [7,6,5,4,3,2,1]
-    sorted_list = sort_func(test, 0, len(test) - 1)
+    sorted_list = quick_sort_add_sent(test, 0, len(test) - 1)
     print(sorted_list)
