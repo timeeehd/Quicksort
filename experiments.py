d7b0aa4df03313d4aaf1ff0ce25738641fe4e5ea
timeeehd <timeeehd@gmail.com>
2021-09-25 11:14:20 +0200

    Adjusted the experiment file

--- experiments.py
+++ experiments.py
@@ -4,62 +4,67 @@ from utility import *
 
 import matplotlib.pyplot as plt
 import sys
+
 sys.setrecursionlimit(200000)
 
-# Permutation
-# max_i: int = 20
-# N: int = 10
-# start: float = time.time()
-# random shuffled list
-# ns = [int(30 * 1.41 ** i) for i in range(max_i)]
-# args = [generate_input(n) for n in ns]
-# for sublist in args:
-#     random.shuffle(sublist)
+experiment = 'Permutation'
 
-# Reversed list
-# max_i: int = 14
-# N: int = 10
-# start: float = time.time()
-# ns = [int(30 * 1.41 ** i) for i in range(max_i)]
-# args = [generate_input(n) for n in ns]
-# for sublist in args:
-#     sublist.reverse()
-# res_quicksort = benchmark(quick_sort_add_sent, args, N)
-# res_quick_yar = benchmark(sort_func, args, N)
-# end: float = time.time()
+if experiment == 'Permutation':
+    # Permutation
+    max_i: int = 20
+    N: int = 10
+    start: float = time.time()
+    ns = [int(30 * 1.41 ** i) for i in range(max_i)]
+    args = [generate_input(n) for n in ns]
+    for sublist in args:
+        random.shuffle(sublist)
+    res_quicksort = benchmark(quick_sort_add_sent, args, N)
+    res_quick_yar = benchmark(sort_func, args, N)
+    end: float = time.time()
+elif experiment == 'Reversed':
+    # Reversed list
+    max_i: int = 14
+    N: int = 10
+    start: float = time.time()
+    ns = [int(30 * 1.41 ** i) for i in range(max_i)]
+    args = [generate_input(n) for n in ns]
+    for sublist in args:
+        sublist.reverse()
+    res_quicksort = benchmark(quick_sort_add_sent, args, N)
+    res_quick_yar = benchmark(sort_func, args, N)
+    end: float = time.time()
+elif experiment == 'Negative':
+    # Negative list
+    max_i: int = 14
+    N: int = 10
+    start: float = time.time()
+    ns = [int(30 * 1.41 ** i) for i in range(max_i)]
+    args = [generate_negative_input(n) for n in ns]
+    res_quicksort = benchmark(quick_sort_add_sent, args, N)
+    res_quick_yar = benchmark(sort_func, args, N)
+    end: float = time.time()
+elif experiment == 'Equal':
+    # Equal list
+    max_i: int = 16
+    N: int = 10
+    start: float = time.time()
+    ns = [int(30 * 1.41 ** i) for i in range(max_i)]
+    args = [generate_equal_input(n) for n in ns]
+    res_quicksort = benchmark(quick_sort_add_sent, args, N)
+    res_quick_yar = benchmark(sort_func, args, N)
+    end: float = time.time()
+else:
+    # Random
+    max_i: int = 20
+    N: int = 10
+    start: float = time.time()
+    ns = [int(30 * 1.41 ** i) for i in range(max_i)]
+    args = [generate_random_input(n) for n in ns]
+    res_quicksort = benchmark(quick_sort_add_sent, args, N)
+    res_quick_yar = benchmark(sort_func, args, N)
 
-# Negative list
-# max_i: int = 14
-# N: int = 10
-# start: float = time.time()
-# ns = [int(30 * 1.41 ** i) for i in range(max_i)]
-# args = [generate_negative_input(n) for n in ns]
-# res_quicksort = benchmark(quick_sort_add_sent, args, N)
-# res_quick_yar = benchmark(sort_func, args, N)
-# end: float = time.time()
-
-# Equal list
-# max_i: int = 16
-# N: int = 10
-# start: float = time.time()
-# ns = [int(30 * 1.41 ** i) for i in range(max_i)]
-# args = [generate_equal_input(n) for n in ns]
-# res_quicksort = benchmark(quick_sort_add_sent, args, N)
-# res_quick_yar = benchmark(sort_func, args, N)
-# end: float = time.time()
-
-# Random
-max_i: int = 20
-N: int = 10
-start: float = time.time()
-ns = [int(30 * 1.41 ** i) for i in range(max_i)]
-args = [generate_random_input(n) for n in ns]
-res_quicksort = benchmark(quick_sort_add_sent, args, N)
-res_quick_yar = benchmark(sort_func, args, N)
-
-
-write_latex_tabular(ns, res_quicksort, "quicksort_tabular.tex")
-write_latex_tabular(ns, res_quick_yar, "quicksort_Yaroslavskiy_tabular.tex")
+write_latex_tabular(ns, res_quicksort, f'quicksort_tabular_{experiment}.tex')
+write_latex_tabular(ns, res_quick_yar, f'quicksort_Yaroslavskiy_tabular_{experiment}.tex')
 
 fig = plt.figure()
 ax = fig.gca()
@@ -69,5 +74,5 @@ ax.set_xlabel('Number of elements $n$')
 ax.set_ylabel('Time (s)')
 ax.set_yscale('log')
 ax.legend(['Quicksort algorithm', 'Quicksort Yaroslavskiy algorithm'])
-# plt.savefig('plot_cubic_vs_hashmap_log.pdf')
+plt.savefig(f'plot_cubic_vs_hashmap_{experiment}.pdf')
 plt.show()
