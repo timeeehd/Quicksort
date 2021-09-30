d62f2f6c3971a2c319c945d9cebbe2bc4c88783e
timeeehd <timeeehd@gmail.com>
2021-09-30 15:07:29 +0200

    Update in experiments.py

--- experiments.py
+++ experiments.py
@@ -1,3 +1,5 @@
+import threading
+
 from secondQuicksort import *
 from firstQuicksort import *
 from utility import *
@@ -6,8 +8,9 @@ import matplotlib.pyplot as plt
 import sys
 
 sys.setrecursionlimit(200000)
+# threading.stack_size(32768*8)
 
-experiment = 'Permutation'
+experiment = 'Random'
 
 if experiment == 'Permutation':
     # Permutation
@@ -76,3 +79,49 @@ ax.set_yscale('log')
 ax.legend(['Quicksort algorithm', 'Quicksort Yaroslavskiy algorithm'])
 plt.savefig(f'plot_cubic_vs_hashmap_{experiment}.pdf')
 plt.show()
+
+
+def benchmark_py(args: List[List[int]], N: int) -> np.ndarray:
+    m: int = len(args)
+    M: np.ndarray = np.zeros((m, N))
+    for i in range(len(args)):
+        print(i)
+        arg: List[int] = args[i]
+        for j in range(N):
+            start: float = time.time()
+            arg.sort()
+            end: float = time.time()
+            M[i, j] = end - start
+
+    means = np.mean(M, axis=1).reshape(m, 1)
+    stdevs = np.std(M, axis=1, ddof=1).reshape(m, 1)
+    return np.stack([means, stdevs])
+
+
+experiment = 'Benchmark'
+
+if experiment == 'Benchmark':
+    # Random
+    max_i: int = 20
+    N: int = 10
+    start: float = time.time()
+    ns = [int(30 * 1.41 ** i) for i in range(max_i)]
+    args = [generate_random_input(n) for n in ns]
+
+    res_quick_yar = benchmark(sort_func, args, N)
+    benchmark_py_sort = benchmark_py(args, N)
+
+    write_latex_tabular(ns, benchmark_py_sort, f'benchmark_python_sort{experiment}.tex')
+    write_latex_tabular(ns, res_quick_yar, f'quicksort_Yaroslavskiy_tabular_{experiment}.tex')
+
+    fig = plt.figure()
+    ax = fig.gca()
+    ax.errorbar(ns, benchmark_py_sort[0, :].reshape(len(ns)), benchmark_py_sort[1, :].reshape(len(ns)), capsize=3.0,
+                marker='o')
+    ax.errorbar(ns, res_quick_yar[0, :].reshape(len(ns)), res_quick_yar[1, :].reshape(len(ns)), capsize=3.0, marker='o')
+    ax.set_xlabel('Number of elements $n$')
+    ax.set_ylabel('Time (s)')
+    ax.set_yscale('log')
+    ax.legend(['Python Sort', 'Quicksort Yaroslavskiy algorithm'])
+    plt.savefig(f'plot_python_vs_yaroslavski_{experiment}.pdf')
+    plt.show()
